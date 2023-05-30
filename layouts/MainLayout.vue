5ee0fb3b60792c409b74ad8285c9a23f353a55cc
roman.messieo <messineoroman@gmail.com>
2023-05-30 20:26:43 -0300

    aliExpressCone nav & header

---
+++ layouts/MainLayout.vue
@@ -0,0 +1,121 @@
+<template>
+  <div id="MainLayout" class="w-full fixed z-50">
+    <div id="TopMenu" class="w-full bg[#FAFAFA] border-b md:block hidden">
+      <ul
+        class="flex items-center justify-end text-xs text-[#333333] font-light px-2 h-10 bg-[#FAFAFA] max-w-[1200px]"
+      >
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Sell on AliExpress
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Cookie Preferenses
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Help
+        </li>
+        <li
+          class="border-r border-r-gray-400 px-3 hover:text-[#FF4646] cursor-pointer"
+        >
+          Buyer Protection
+        </li>
+        <li class="px-3 hover:text-[#FF4646] cursor-poiner">
+          <Icon name="ic:sharp-install-mobile" size="17" />
+          App
+        </li>
+
+        <li
+          @mouseenter="isAccountMenu = true"
+          @mouseleave="isAccountMenu = false"
+          class="relative flex items-center px-2.5 hover:text-[#FF4646] h-full cursor-pointer"
+          :class="
+            isAccountMenu
+              ? 'bg-white border z-40 shadow-[0_15px_100px_40px_rgba(0,0,0,0.3)]'
+              : 'border border-[#FAFAFA]'
+          "
+        >
+          <Icon name="ph:user-thin" size="17" />
+          Account
+          <Icon name="mdi:chevron-down" size="15" class="ml-5" />
+          <div
+            id="AccountMenu"
+            v-if="isAccountMenu"
+            class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
+          >
+            <div v-if="true">
+              <div class="text-semibold text-[15px] my-4 px-3">
+                Welcome to AliExprees!
+              </div>
+              <div class="flex items-center gap-1 px-3 mb-3">
+                <nuxt-link
+                  to="/auth"
+                  class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
+                >
+                  Login / Register
+                </nuxt-link>
+              </div>
+            </div>
+            <div class="border-b" />
+            <ul class="bg-white">
+              <li
+                @click="navegateTo('/orders')"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                My Orders
+              </li>
+              <li
+                v-if="true"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                Sign out
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div id="MainHeader" class="flex items-center w-full bg-white">
+      <div
+        class="flex lg:jusfy-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+      >
+        <nuxt-link to="/" class="min-w-[170px]">
+          <img width="170" src="/AliExpress-logo.png" alt="" />
+        </nuxt-link>
+
+        <div class="max-w-[700px] w-full md:block hidden">
+          <div class="relative">
+            <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+              <input
+               class="
+               w-full
+               placeholder-gray-400
+               text-sm
+               pl-3
+               focus:outline-none
+               "
+              placeholder="kitchen accessories"
+              type="text"
+              v-model="searchItem">
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+let isAccountMenu = ref(false);
+let searchItem = ref('')
+</script>
+
+<!-- <style lang="scss" scoped>
+
+</style> -->
