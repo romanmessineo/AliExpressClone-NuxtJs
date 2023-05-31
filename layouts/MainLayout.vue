e36121550a69003445bcc02d178b7e8a23471e7f
roman.messieo <messineoroman@gmail.com>
2023-05-31 15:11:10 -0300

    searchbar-footer

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -52,12 +52,12 @@
                 Welcome to AliExprees!
               </div>
               <div class="flex items-center gap-1 px-3 mb-3">
-                <nuxt-link
+                <NuxtLink
                   to="/auth"
                   class="bg-[#FF4646] text-center w-full text-[16px] rounded-sm text-white font-semibold p-2"
                 >
                   Login / Register
-                </nuxt-link>
+                </NuxtLink>
               </div>
             </div>
             <div class="border-b" />
@@ -83,37 +83,102 @@
       <div
         class="flex lg:jusfy-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
       >
-        <nuxt-link to="/" class="min-w-[170px]">
+        <NuxtLink to="/" class="min-w-[170px]">
           <img width="170" src="/AliExpress-logo.png" alt="" />
-        </nuxt-link>
+        </NuxtLink>
 
         <div class="max-w-[700px] w-full md:block hidden">
           <div class="relative">
-            <div class="flex items-center border-2 border-[#FF4646] rounded-md w-full">
+            <div
+              class="flex items-center border-2 border-[#FF4646] rounded-md w-full"
+            >
               <input
-               class="
-               w-full
-               placeholder-gray-400
-               text-sm
-               pl-3
-               focus:outline-none
-               "
-              placeholder="kitchen accessories"
-              type="text"
-              v-model="searchItem">
+                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                placeholder="kitchen accessories"
+                type="text"
+                v-model="searchItem"
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button
+                class="flex items-center h-[100%] p-1.5 px-2 bg-[#FF4646]"
+              >
+                <Icon name="ph:magnifying-glass" size="20" color="#ffffff" />
+              </button>
+            </div>
+
+            <div
+              class="absolute bg-white max-white max-w-[700px] h-auto w-full"
+            >
+              <div v-if="false" class="p-1">
+                <NuxtLink
+                  to="`/item/1`"
+                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img
+                      class="rounded-md"
+                      width="40"
+                      src="/cart-empty.png"
+                      alt=""
+                    />
+                    <div class="trubcate ml-2">TESTING</div>
+                  </div>
+                  <div class="truncate">$ 98.99</div>
+                </NuxtLink>
+              </div>
             </div>
           </div>
-
-
         </div>
+
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#FF4646' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+        <button
+          @click="userStore.isMenuOverlay = true"
+          class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+        >
+          <Icon name="radix-icons:hamburger-menu" size="33" />
+        </button>
       </div>
     </div>
   </div>
+
+   <Loading v-if="userStore.isLoading" />
+  <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]" />
+  <slot />
+
+  <Footer v-if="!userStore.isLoading"/>
 </template>
 
 <script setup>
+import { useUserStore } from '~/stores/user';
+const userStore = useUserStore()
 let isAccountMenu = ref(false);
-let searchItem = ref('')
+let isCartHover = ref(false);
+let isSearching = ref(true);
+let searchItem = ref("");
 </script>
 
 <!-- <style lang="scss" scoped>
