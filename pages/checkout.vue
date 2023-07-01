82173f509056d60fa89b22e6db1a507c8530a17a
roman.messieo <messineoroman@gmail.com>
2023-07-01 18:15:48 -0300

    update

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -217,9 +217,9 @@ const pay = async () => {
         isProcessing.value = false
     } else {
         await createOrder(result.paymentIntent.id)
-        userStore.cart = []
-        userStore.checkout = []
         setTimeout(() => {
+            userStore.cart = []
+            userStore.checkout = []
             return navigateTo('/success')
         }, 500)
     }
